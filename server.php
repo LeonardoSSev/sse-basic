df453b78b9c12033df3a09f2b3566e012116d665
Leonardo Santos Severino <leonardossev@gmail.com>
2019-09-29 14:11:25 -0300

    Sends date events

--- server.php
+++ server.php
@@ -3,3 +3,23 @@
 date_default_timezone_set("America/Sao_Paulo");
 header("Cache-Control: no-cache");
 header("Content-Type: text/event-stream\n\n");
+
+$counter = rand(1, 10);
+
+while (true) {
+    echo "event: ping\n";
+    $currentDate = date(DATE_ISO8601);
+    echo 'data: {"time": ' . $currentDate . '"}';
+    echo "\n\n";
+
+    $counter--;
+
+    if (!$counter) {
+        echo "data: This is a message at time " . $currentDate . "\n\n";
+        $counter = rand(1, 10);
+    }
+
+    ob_end_flush();
+    flush();
+    sleep(1);
+}
